2fb2e628ee28a292a3c5b9f3f203bfbc184afc77
Shion Ichikawa <shion1305@gmail.com>
2024-10-26 13:24:16 +0900

    🚧 print dbUrl

--- backend/cmd/dev/main.go
+++ backend/cmd/dev/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"backend/pkg/handler"
+	"backend/pkg/settings"
+	"backend/svc/pkg/handler"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
 	r := gin.Default()
-
+	conf := settings.Get()
+	var dbUrl string
+	switch conf.Infrastructure.Postgres.Protocol {
+	case "tcp":
+		dbUrl = fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable&TimeZone=Asia/Tokyo",
+			conf.Infrastructure.Postgres.User,
+			conf.Infrastructure.Postgres.Password,
+			conf.Infrastructure.Postgres.Host,
+			conf.Infrastructure.Postgres.Port,
+			conf.Infrastructure.Postgres.DBName)
+	case "unix":
+		dbUrl = fmt.Sprintf("postgresql://%s:%s@unix:%s/%s?sslmode=disable&TimeZone=Asia/Tokyo",
+			conf.Infrastructure.Postgres.User,
+			conf.Infrastructure.Postgres.Password,
+			conf.Infrastructure.Postgres.UnixSocket,
+			conf.Infrastructure.Postgres.DBName)
+	default:
+		log.Fatalf("invalid protocol: %s", conf.Infrastructure.Postgres.Protocol)
+	}
+	fmt.Println(dbUrl)
 	// Health Check
 	h := handler.NewHealthHandler()
 	r.GET("/health", h.Handle)
