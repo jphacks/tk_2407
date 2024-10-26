bfb172b810a93bffffc8a8a40cc513c4660d06a0
ryo_sasaki <118416144+Ryo-Sasaki-xxx@users.noreply.github.com>
2024-10-27 04:52:57 +0900

    とりまロジック

--- frontend/src/components/CustomMarker.tsx
+++ frontend/src/components/CustomMarker.tsx
@@ -1,13 +1,35 @@
 import { Location } from '@/types/Location'
-import { AdvancedMarker } from '@vis.gl/react-google-maps'
+import {
+  AdvancedMarker,
+  InfoWindow,
+  useAdvancedMarkerRef,
+} from '@vis.gl/react-google-maps'
 import { CustomPin } from '@/components/CustomPin'
+import { useState } from 'react'
 
 export function CustomMarker({ location }: { location: Location }) {
+  const [isOpenInfowindow, setIsOpenInfowindow] = useState<boolean>(false)
+  const [markerRef, marker] = useAdvancedMarkerRef()
+
   return (
     <AdvancedMarker
+      ref={markerRef}
+      onClick={() =>
+        setIsOpenInfowindow((isOpenInfowindow) => !isOpenInfowindow)
+      }
       position={{ lat: location.latitude, lng: location.longitude }}
+      title={'AdvancedMarker that opens an Infowindow when clicked.'}
     >
       <CustomPin imageUrl="/saru.png" />
+      {isOpenInfowindow && (
+        <InfoWindow
+          anchor={marker}
+          maxWidth={200}
+          onCloseClick={() => setIsOpenInfowindow(false)}
+        >
+          This is an example for the combined with an Infowindow.
+        </InfoWindow>
+      )}
     </AdvancedMarker>
   )
 }
