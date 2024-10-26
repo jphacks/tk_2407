fc6bf84993943c05e545d476ec5b0cc130bf3733
pirotyyy <hiroki1423@docomo.ne.jp>
2024-10-26 18:00:54 +0900

    :sparkles: commit

--- frontend/src/features/moc/MocPage.tsx
+++ frontend/src/features/moc/MocPage.tsx
@@ -1,7 +1,30 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
-import React from 'react'
+
+import React, {useEffect} from 'react'
+import api from "@/api/$api";
+import aspida from '@aspida/fetch';
+
+// to Rare-san
+
+// 1. pnpm i @aspida/fetch
+// 2. apiClientを作成（別ファイルでやって他からimportするかたちがいいかも）
+//      import api from "@/api/$api";
+//      import aspida from '@aspida/fetch';
+//      const apiClient = api(aspida(undefined, {baseURL: 'http://localhost:8080/'}))
+// 3. Codeを参考に使い方をみてもらえれば3150
+
 
 export default function MocPage() {
+    const apiClient = api(aspida(undefined, {baseURL: 'http://localhost:8080/'}))
+  useEffect(() => {
+      const fechData = async () => {
+          const res = await apiClient.api.v1.health.get()
+          console.log(res)
+      }
+      fechData()
+  }, [])
   return (
     <>
       <h1>
