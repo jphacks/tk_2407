58a89ea7a9e6e7c0756f3991af235a72472eac7a
ryo_sasaki <118416144+Ryo-Sasaki-xxx@users.noreply.github.com>
2024-10-27 11:52:25 +0900

    ページ遷移させたよーん

--- frontend/src/components/SpeechBubble.tsx
+++ frontend/src/components/SpeechBubble.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 interface SpeechBubbleProps {
   description: string
@@ -10,8 +11,8 @@ export function SpeechBubble({ description }: SpeechBubbleProps) {
     <Card className="bg-white rounded-xl border-none">
       <CardContent className="p-1">
         <p className="text-sm break-words text-gray-600 mb-2">{description}</p>
-        <Button className="w-full bg-[#f87171] hover:bg-[#ef4444] text-white">
-          よせがきを見る
+        <Button className="w-full bg-[#f87171] hover:bg-[#ef4444] text-white ">
+          <Link href="/message">よせがきを見る</Link>
         </Button>
       </CardContent>
     </Card>
