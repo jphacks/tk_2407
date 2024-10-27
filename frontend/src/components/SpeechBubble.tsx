b50ecbab070378a73c3dbdd3c42facdf5072f261
Shion Ichikawa <shion1305@gmail.com>
2024-10-27 15:41:45 +0900

    ✨ 連携完了

--- frontend/src/components/SpeechBubble.tsx
+++ frontend/src/components/SpeechBubble.tsx
@@ -2,18 +2,19 @@ import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
-interface SpeechBubbleProps {
-  lint: string
+export function SpeechBubble({
+  url,
+  description,
+}: {
+  url: string
   description: string
-}
-
-export function SpeechBubble({ link, description }: SpeechBubbleProps) {
+}) {
   return (
     <Card className="bg-white rounded-xl border-none">
       <CardContent className="p-1">
         <p className="text-sm break-words text-gray-600 mb-2">{description}</p>
         <Button className="w-full bg-[#f87171] hover:bg-[#ef4444] text-white ">
-          <Link href={link}>よせがきを見る</Link>
+          <Link href={url}>よせがきを見る</Link>
         </Button>
       </CardContent>
     </Card>
